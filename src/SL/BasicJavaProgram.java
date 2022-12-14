f92681d1da68880ee3e686e36a80c16c28bc9fb1
Manoj Kumar <manojkr55@gmail.com>
2022-12-14 19:32:49 +0530

    Update for while loop

--- src/SL/BasicJavaProgram.java
+++ src/SL/BasicJavaProgram.java
@@ -18,10 +18,28 @@ public class BasicJavaProgram {
 		
 		System.out.println("Arthmatic operations on the two numbers are : ");
 		System.out.println("Addition of the numbers is : "+ (num1 + num2) );
-		System.out.println("Substraction of the numbers is : " + (num2 - num1));
-		System.out.println("Multiplication of the numbers is : " + (num1 * num2));
-		System.out.println("Division of the numbers is : " + (num2 / num1));
-		System.out.println("Modulo of the numbers is : " + (num2 % num1));
+//		System.out.println("Subtraction of the numbers is : " + (num2 - num1));
+//		System.out.println("Multiplication of the numbers is : " + (num1 * num2));
+//		System.out.println("Division of the numbers is : " + (num2 / num1));
+//		System.out.println("Modulo of the numbers is : " + (num2 % num1));
+		
+		int fact_var1 = 1;
+		
+		// For loop
+		for(int i = 1; i <= num1; i++ ) {
+			fact_var1 = fact_var1 * i;
+		}
+		
+		System.out.println("Factorial of the num1 is : " + fact_var1);
+		
+		int j = 1;
+		int fact_var2 = 1;
+		// While loop
+		while( j <= num2) {
+			fact_var2 *= j;
+			j++;
+		}
+		
+		System.out.println("Factorial of the num2 is : " + fact_var2);
 	}
-
 }
